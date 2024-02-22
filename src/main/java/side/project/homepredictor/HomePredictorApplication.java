dc57a06a06408b578aae8d993c22fede0121768a
tjdtn0219 <s_sjang@naver.com>
2024-02-22 20:46:36 +0900

    Init Project

---
+++ src/main/java/side/project/homepredictor/HomePredictorApplication.java
@@ -0,0 +1,13 @@
+package side.project.homepredictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomePredictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomePredictorApplication.class, args);
+	}
+
+}
